776f477999cd9e11c97daddf1ecac0d537426ecb
Leandro Trinidad Santos <leandro_trinidad@ucne.edu.do>
2021-06-02 19:24:22 -0400

    Agregando mensaje de error en la clase producto

---
+++ Parcial1-AP2/BLL/ProductosBLL.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_AP2.DAL;
+using Parcial1_AP2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Parcial1_AP2.BLL
+{
+    
+}
--- Parcial1-AP2/Models/Productos.cs
+++ Parcial1-AP2/Models/Productos.cs
@@ -8,10 +8,19 @@ namespace Parcial1_AP2.Models
 {
     public class Productos
     {
+        [Key]
         public int ProductoId { get; set; }
+
+        [Required(ErrorMessage ="El campo Descripccion esta vacio")]
         public string Descripccion { get; set; }
+
+        [Required(ErrorMessage ="El campo Existencia esta vacio")]
         public int Existencia { get; set; }
-        public double Costo { get; set; }
-        public double Valor_Inventario { get; set; }
+
+        [Required(ErrorMessage = "El campo Costo esta vacio")]
+        public decimal Costo { get; set; }
+
+        [Required(ErrorMessage =" El campo Valor Inventario esta vacio")]
+        public decimal Valor_Inventario { get; set; }
     }
 }
