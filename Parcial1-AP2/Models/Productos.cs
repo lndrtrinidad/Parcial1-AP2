04ac1d2b2c00a637b2d7193b5085b325d63a7609
Leandro Trinidad Santos <leandro_trinidad@ucne.edu.do>
2021-06-02 18:33:14 -0400

    Clase producto creada

---
+++ Parcial1-AP2/Models/Productos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_AP2.Models
+{
+    public class Productos
+    {
+        [key]
+        public int ProductoId { get; set; }
+        public string Descripccion { get; set; }
+        public int Existencia { get; set; }
+        public double Costo { get; set; }
+        public double Valor_Inventario { get; set; }
+    }
+}
